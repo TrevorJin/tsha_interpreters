1543eba618377deb720d21759d71a647d194c6a2
Trevor Jin <ph_201@yahoo.com>
2016-03-07 13:46:37 -0600

    Use SSL and the Puma webserver in production.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:show, :index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :manager_user,   only: :index
   before_action :admin_user,     only: [:promote_to_manager, :promote_to_admin, :destroy]
