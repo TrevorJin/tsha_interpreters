545e09e5c41c5ed890196f919aebe07e14483f2f
Trevor Jin <ph_201@yahoo.com>
2016-03-22 11:16:48 -0500

    Manager dashboard tests.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,10 +12,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     log_in_as(@admin)
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", dashboard_path, count: 1
     assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", jobs_path, count: 1
-    assert_select "a[href=?]", new_job_path, count: 1
     assert_select "a[href=?]", user_path(@admin), count: 1
     assert_select "a[href=?]", edit_user_path(@admin), count: 1
     assert_select "a[href=?]", about_path
@@ -30,9 +29,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     log_in_as(@manager)
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", dashboard_path, count: 1
     assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", new_job_path, count: 1
     assert_select "a[href=?]", user_path(@manager), count: 1
     assert_select "a[href=?]", edit_user_path(@manager), count: 1
     assert_select "a[href=?]", about_path
@@ -47,7 +46,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     log_in_as(@regular_user)
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path,  count: 2
+    assert_select "a[href=?]", root_path,  count: 1
     assert_select "a[href=?]", help_path,  count: 1
     assert_select "a[href=?]", user_path(@regular_user), count: 1
     assert_select "a[href=?]", edit_user_path(@regular_user), count: 1
@@ -63,7 +62,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links when not logged in" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path,  count: 2
+    assert_select "a[href=?]", root_path,  count: 1
     assert_select "a[href=?]", help_path,  count: 1
     assert_select "a[href=?]", login_path, count: 1
     assert_select "a[href=?]", "http://www.tsha.cc/"
@@ -73,5 +72,33 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", "http://www.tsha.cc/resources/", count: 1
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "manager dashboard for admin" do
+    log_in_as(@admin)
+    get dashboard_path
+    assert_template 'users/dashboard'
+    assert_select "a[href=?]", dashboard_path, count: 2
+    assert_select "a[href=?]", pending_users_path, count: 2
+    assert_select "a[href=?]", interpreters_path, count: 2
+    assert_select "a[href=?]", interpreters_new_path, count: 1
+    assert_select "a[href=?]", jobs_path, count: 2
+    assert_select "a[href=?]", new_job_path, count: 1
+    assert_select "a[href=?]", customers_path, count: 2
+    assert_select "a[href=?]", new_customer_path, count: 1
+  end
+
+  test "manager dashboard for manager" do
+    log_in_as(@manager)
+    get dashboard_path
+    assert_template 'users/dashboard'
+    assert_select "a[href=?]", dashboard_path, count: 2
+    assert_select "a[href=?]", pending_users_path, count: 2
+    assert_select "a[href=?]", interpreters_path, count: 2
+    assert_select "a[href=?]", interpreters_new_path, count: 1
+    assert_select "a[href=?]", jobs_path, count: 2
+    assert_select "a[href=?]", new_job_path, count: 1
+    assert_select "a[href=?]", customers_path, count: 2
+    assert_select "a[href=?]", new_customer_path, count: 1
   end
 end
