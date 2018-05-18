f5cdf0e9d479c778b0583a8a8ba4c66d94e69c31
Trevor Jin <ph_201@yahoo.com>
2018-05-17 21:20:18 -0500

    Removes auto expiring of job requests

--- app/helpers/job_status_helper.rb
+++ app/helpers/job_status_helper.rb
@@ -1,13 +1,4 @@
 module JobStatusHelper
-  def mark_expired_job_requests
-    @non_expired_active_job_requests = JobRequest.where("awaiting_approval = ? AND expired = ?", true, false)
-    @non_expired_active_job_requests.each do |non_expired_active_job_request|
-      if non_expired_active_job_request.start < Time.now
-        non_expired_active_job_request.expire_job_request
-      end
-    end
-  end
-
   def mark_completed_jobs
     @confirmed_and_completed_jobs = Job.where("has_interpreter_assigned = ? AND completed = ? AND invoice_submitted = ? AND expired = ?", true, false, false, false)
     
@@ -24,7 +15,6 @@ module JobStatusHelper
   end
 
   def update_job_and_job_request_statuses
-    mark_expired_job_requests
     mark_completed_jobs
   end
 end
