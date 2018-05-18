dfb88b23caa274a9b75f722695b8788710b103cb
Trevor Jin <ph_201@yahoo.com>
2018-05-17 20:51:30 -0500

    Removes auto-expiring code

--- app/helpers/job_status_helper.rb
+++ app/helpers/job_status_helper.rb
@@ -1,13 +1,4 @@
 module JobStatusHelper
-  def mark_expired_jobs
-    @non_expired_active_jobs = Job.where("has_interpreter_assigned = ? AND expired = ?", false, false)
-    @non_expired_active_jobs.each do |non_expired_active_job|
-      if non_expired_active_job.start < Time.now
-        non_expired_active_job.expire_job
-      end
-    end
-  end
-
   def mark_expired_job_requests
     @non_expired_active_job_requests = JobRequest.where("awaiting_approval = ? AND expired = ?", true, false)
     @non_expired_active_job_requests.each do |non_expired_active_job_request|
@@ -33,7 +24,6 @@ module JobStatusHelper
   end
 
   def update_job_and_job_request_statuses
-    mark_expired_jobs
     mark_expired_job_requests
     mark_completed_jobs
   end
